e789ec3331249c6dd2cb1f4e3da6290327f359c8
radikhayriyan1 <radikhayriyan1@gmail.com>
2024-03-17 16:44:19 +0400

    Init project

---
+++ src/main/java/com/javarush/island/hayriyan/App.java
@@ -0,0 +1,13 @@
+package com.javarush.island.hayriyan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
